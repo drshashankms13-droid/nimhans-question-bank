ce360f294e16e8a7bf0b01f087481b48bab06e4c
Arthur Avalon <dr.shashankms13@gmail.com>
2026-07-10 18:26:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 /* ---------- Supabase setup ---------- */
 const SUPABASE_URL = 'https://mxqjvjdjnasavrbbxame.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_ssmaDfCXrAr_PYAOV9glew_4mAgZLH8';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14cWp2amRqbmFzYXZyYmJ4YW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NTk5OTksImV4cCI6MjA5OTIzNTk5OX0.IfEzIz3CQN4Qcd4oclgZxxLroH24p5fw_7k4nSTxu2M';
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentUser = null;
